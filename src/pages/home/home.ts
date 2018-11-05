364dd1409b67cd7d53b17366b3c198887c99b092
alexlu3211 <lujiahong228@gmail.com>
2018-11-04 22:25:20 -0500

    Initial commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,50 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
+
+
+const PLACEHOLDER_IMAGE: string = "/assets/imgs/placeholder.png";
+const SPINNER_IMAGE: string = "/assets/imgs/spinner.gif";
 
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
+
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+	private image = PLACEHOLDER_IMAGE;
+	timeStamp: any;
 
-  }
+	constructor(public navCtrl: NavController,
+				  private camera: Camera) {
+
+	}
+
+
+	takePic() {
+
+		const options: CameraOptions = {
+		  quality: 100,
+		  destinationType: this.camera.DestinationType.DATA_URL,
+		  encodingType: this.camera.EncodingType.JPEG,
+		  mediaType: this.camera.MediaType.PICTURE
+		}
+
+		this.camera.getPicture(options).then((imageData) => {
+			if (imageData) {
+				this.image = 'data:image/jpeg;base64,' + imageData;   
+				this.timeStamp = Date.now();    
+			} else {
+				this.image = PLACEHOLDER_IMAGE;
+				this.timeStamp = null;
+			}
+		}, (err) => {
+			this.image = PLACEHOLDER_IMAGE;
+			this.timeStamp = null;
+		});
+		
+		this.image = SPINNER_IMAGE;
+	}
 
 }
